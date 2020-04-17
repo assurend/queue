39357937d672afb9c3bbdcf0a45bbb06f26d6179
assurend <63834121+assurend@users.noreply.github.com>
2020-04-17 20:31:48 +0530

    Added file description

--- include/queue/queue.h
+++ include/queue/queue.h
@@ -1,11 +1,20 @@
+/**
+ * @file
+ * @brief FIFO Queue header file
+ *
+ * This is the header file for FIFO Queue
+ */
 #ifndef QUEUE_H
 #define QUEUE_H
+
 #include <stdio.h>
 #include <stdbool.h>
+
 bool empty();
 unsigned int size();
 int front();
+int back();
 void push(int data);
-int pop(void);
+void pop();
 
 #endif
