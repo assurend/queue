3f97f0b960445d4ca95d3887673bcee6c8b7d919
assurend <63834121+assurend@users.noreply.github.com>
2020-04-17 20:29:19 +0530

    Added mutex locks

--- src/queue.c
+++ src/queue.c
@@ -1,59 +1,87 @@
-#include <queue/queue.h>
-#include <stdbool.h>
-#include <stdlib.h>
+/**
+ * @file
+ * @brief FIFO Queue
+ *
+ * This file contains the source code for FIFO Queue
+ */
+
+#include <stdliib.h>
+#include <pthread.h>
+#include "queue.h"
+
+#define ERROR -1
+
+static pthread_mutex_t queue_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 typedef struct Node {
-  int data;
-  struct Node *next;
+    int data;
+    struct Node* next;
 } Node;
 
 Node *queueFront = NULL, *queueRear = NULL;
 unsigned int queueCount = 0;
 
+/*Checks whether the Queue is empty*/
 bool empty() { return (queueFront == NULL); }
 
+
+/*Returns the number of entries in the Queue*/
 unsigned int size() { return queueCount; }
 
-int front() {
-  if (!empty()) {
-    return queueFront->data;
-  } else {
-    printf("No elements in Queue");
-    return -1;
-  }
-}
-
-void push(int data) {
-  Node *newEntry = (Node *)malloc(sizeof(Node));
-  if (NULL != newEntry) {
-    newEntry->data = data;
-    newEntry->next = NULL;
-    if (NULL == queueFront) {
-      queueFront = newEntry;
-      queueRear = newEntry;
-    } else if (NULL == queueRear) {
-      queueRear = newEntry;
+/*Access the first element in the Queue*/
+int front()
+{
+    if(!empty()) {
+        return queueFront->data;    
     } else {
-      Node *temp = queueRear;
-      queueRear = newEntry;
-      temp->next = queueRear;
+        printf("No elements in Queue\n");
+        return ERROR;
     }
-    queueCount++;
-  } else {
-    printf("No space left in Queue");
-  }
 }
 
-int pop(void) {
-  if (empty()) {
-    printf("Stack is empty");
-    return -1;
-  }
+/*Access the last element in the Queue*/
+int back()
+{
+    if(!empty()) {
+        return queueRear->data;
+    } else {
+        printf("No elements in Queue\n");
+        return ERROR;
+    }
+}
 
-  Node *temp = queueFront;
-  int a = temp->data;
-  queueFront = queueFront->next;
-  free(temp);
-  queueCount--;
-  return a;
+/*Insert an element to the Queue*/
+void push(int data) {
+    pthread_mutex_lock(&queue_mutex);
+    Node *newEntry = (Node *)malloc(sizeof(Node));
+    if (NULL != newEntry) {
+        newEntry->data = data;
+        newEntry->next = NULL;
+        if (NULL == queueFront) {
+            queueFront = newEntry;
+            queueRear = newEntry;
+        } else {
+            queueRear->next = newEntry;
+            queueRear = newEntry;
+        }
+        queueCount++;
+    } else {
+        printf("No space left in the device\n");
+    }
+    pthread_mutex_unlock(&queue_mutex);
+}
+
+/*Remove the next element from the Queue*/
+void pop() {
+    pthread_mutex_lock(&queue_mutex);
+    if (empty()) {
+        printf("Stack is empty\n");
+        return;
+    }
+
+    Node *temp = queueFront;
+    queueFront = queueFront->next;
+    queueCount--;
+    free(temp);
+    pthread_mutex_unlock(&queue_mutex);
 }
